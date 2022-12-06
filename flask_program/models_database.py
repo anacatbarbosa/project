6df74822b1f204bf648e92574f4463df6e5297d2
leohx2 <leobgse@gmail.com>
2022-12-06 22:08:53 +0000

    Changes to models
    Adding the USER model

--- flask_program/models_database.py
+++ flask_program/models_database.py
@@ -1,4 +1,13 @@
 # A file to code the database models, creating models to store login, images, descriptions, passwords, etc...
+from flask_login import UserMixin
+
 from . import db
 
 # importing the database from the __init__.py, the "from ." means we will import from a file locate at this same folder
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Columun(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(10000), nullable=False)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
--- main_locally_host.py
+++ main_locally_host.py
@@ -5,6 +5,6 @@ from flask_program import creat_app
 app = creat_app()
 
 if __name__ =="__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000)))
+    app.run(debug=True, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000)))
 
 #A main file to make it easier to run locally.
