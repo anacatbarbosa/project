10885b6c77b0f6197347a031ef9944c8c8872003
leohx2 <leobgse@gmail.com>
2022-12-08 10:58:32 +0000

    removing test.html
    Removing test.html
    turning some variables path global

--- flask_program/views.py
+++ flask_program/views.py
@@ -6,19 +6,24 @@ from flask import (Blueprint, flash, redirect, render_template, request,
 from werkzeug.utils import secure_filename
 
 from . import db
+from .models_database import Post, User
 
 #Blueprint allow to code the views, or @app.routes, in multiple files
 
 views = Blueprint('views', __name__) #setting the Blueprint variable name.
+upload_folder = 'flask_program/static/uploaded_files'# path to the uploaded folder to save the files. Path from main.py to upoladed_files
+pic_to_html =  '../static/uploaded_files' #path to pass to html file, this path + filename will inform the url to the html go take it
+#path_save = os.path.join(upload_folder, filename)
 
 @views.route('/')
 def index():
     return render_template('index.html')
 
+
+
+
 @views.route('/test', methods=['GET', 'POST'])
 def test():
-
-    from .models_database import Post, User
 
     if request.method == 'POST':
         pic = request.files['file']
@@ -31,7 +36,7 @@ def test():
         upload_folder = 'flask_program/static/uploaded_files'
         path_save = os.path.join(upload_folder, filename)
         pic.save(path_save)
-        path_html = os.path.join('../static/uploaded_files', filename)
+        path_html = os.path.join(pic_to_html, filename)
         post = Post(img_path=path_html, filename=filename, mimetype=mimetype, user_id=1)
         db.session.add(post)
         db.session.commit()
