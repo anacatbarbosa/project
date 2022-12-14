f93f708a456a321082ee64a8d7826c62dbb34c94
leohx2 <leobgse@gmail.com>
2022-12-14 15:51:09 +0000

    Creating a login route
    Creating a login rout without information or utility, just to finish the htlm and the css of the page

--- flask_program/auth.py
+++ flask_program/auth.py
@@ -1,8 +1,14 @@
 #File to code authentication files routes, such as login and register.
 from flask import (  # Blueprint allow to code the views, or @app.routes, in multiple files
     Blueprint, flash, redirect, render_template, request, session, url_for)
+from werkzeug.security import check_password_hash, generate_password_hash
 
 #Blueprint allow to code the views, or @app.routes, in multiple files
 
 auth = Blueprint('auth', __name__) #setting the Blueprint variable name.
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        a = 'test'
+    render_template('login.html')
