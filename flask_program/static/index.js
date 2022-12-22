89e7111414bce65eb9319c0a77473a991980d11a
Catarina Barbosa <100435667+anacatbarbosa@users.noreply.github.com>
2022-12-22 08:34:40 +0000

    JS changes infinity scroll

--- flask_program/static/index.js
+++ flask_program/static/index.js
@@ -2,13 +2,24 @@ const container = document.querySelector('.container');
 
 let post_details = 0;
 
-function loadImages (numImages = 9) {
-    
-
-}
-
 fetch('/get_posts', {
     method: 'POST'
 })
     .then(res => res.json())
     .then(data => console.log(data))
+
+function loadImages (numImages = 9) {
+    let i = 0;
+    while (i < numImages) {
+        const img = document.createElement('img')
+        i++;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        post_details++;
+        console.log(post_details)
+    }
+})
+
