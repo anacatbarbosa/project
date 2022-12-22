513e2c5560aad753bfe609fc981ee7e3383548b2
Catarina Barbosa <100435667+anacatbarbosa@users.noreply.github.com>
2022-12-22 16:53:34 +0000

    CSS class changes and JS changes

--- flask_program/static/index.js
+++ flask_program/static/index.js
@@ -1,4 +1,11 @@
 const container = document.querySelector('#infinity');
+
+/* number of recipes displayed at first when accessing recipes page */
+const firstRecipes = 9
+
+/* number of recipes shown when hitting the end of page and enabling infinity scroll */
+const scrollRecipes = 6
+
 /* signaling the back end to receive a json file with all the post information to show on page */
 async function fetchData(){
     let response = await fetch('/get_posts', {method: 'POST'});
@@ -12,16 +19,22 @@ let scroll_counter = 0;
 
 /* function to load numImages to show on #infinity div at recipes.html 
    post_details contains path, titles and post_id*/
-async function loadImages (already_uploaded, numImages = 2) {
+async function loadImages (already_uploaded, numImages = firstRecipes) {
     const post_details = await fetchData()
     const total_posts = post_details['path'].length /* counts every post */
     let i = already_uploaded;
     numImages += already_uploaded
+
     /* loop breaks when total posts are reached */
     while (i < numImages && i < total_posts) {
-        const img = document.createElement('img')
-        img.src = post_details["path"][i]
-        container.appendChild(img);
+        const buttonEl = document.createElement('button')
+        buttonEl.classList.add('col-sm')
+        buttonEl.innerHTML = `
+            <h4>${post_details['titles'][i]}</h4>
+            <img src=${post_details['path'][i]}>
+            <input type="hidden" name="post_id" value=${post_details['post_id'][i]}>
+        `
+        container.appendChild(buttonEl)
         i++;
     }
 }
@@ -31,7 +44,13 @@ loadImages(0);
 window.addEventListener('scroll', () => {
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
         scroll_counter++
-        loadImages(scroll_counter * 2);
+        if (scroll_counter == 1) {
+            loadImages(firstRecipes, numImages = scrollRecipes)
+        }
+        else
+        {
+            loadImages(firstRecipes + scrollRecipes * (scroll_counter - 1), numImages = scrollRecipes)
+        }
     }
 })
 
