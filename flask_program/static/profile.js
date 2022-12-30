40139ecf7c63793c8c697e2d7991c17a3656e187
Catarina Barbosa <100435667+anacatbarbosa@users.noreply.github.com>
2022-12-30 18:07:41 +0000

    profile changes
    style changes and form element changes for back-end purposes

--- flask_program/static/profile.js
+++ flask_program/static/profile.js
@@ -10,15 +10,15 @@ function changeName() {
         <div class="changeName">
             <form method="post" action="/profile/name">
                 <div class="form-group row">
-                    <label for="oldName" class="col-sm-2 col-form-label">Old Username</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" name="oldName" id="oldName" placeholder="Old Username">
-                    </div>
-                </div>
-                <div class="form-group row">
                     <label for="newName" class="col-sm-2 col-form-label">New Username</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" name="newName" id="newName" placeholder="New Username">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-2 col-form-label">Confirm Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Confirm Password">
                     </div>
                 </div>
                 <div class="form-group row">
@@ -36,15 +36,15 @@ function changeEmail() {
         <div class="changeEmail">
             <form method="post" action="/profile/email">
                 <div class="form-group row">
-                    <label for="oldEmail" class="col-sm-2 col-form-label">Old E-mail</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" name="oldEmail" id="oldEmail" placeholder="Old E-mail">
-                    </div>
-                </div>
-                <div class="form-group row">
                     <label for="newEmail" class="col-sm-2 col-form-label">New E-mail</label>
                     <div class="col-sm-10">
                         <input type="email" class="form-control" name="newEmail" id="newEmail" placeholder="New E-mail">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-2 col-form-label">Confirm Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Confirm Password">
                     </div>
                 </div>
                 <div class="form-group row">
@@ -62,9 +62,9 @@ function pwChange() {
         <div class="changePw">
             <form method="post" action="/profile/password">
                 <div class="form-group row">
-                    <label for="oldPw" class="col-sm-2 col-form-label">Old Password</label>
+                    <label for="password" class="col-sm-2 col-form-label">Old Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" name="oldPw" id="oldPw" placeholder="Old Password">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Old Password">
                     </div>
                 </div>
                 <div class="form-group row">
